dec0f0d40b9da1f614262f83020675a44705e28c
Kevin <playsbykevin@gmail.com>
2023-05-20 23:30:00 +0300

    init commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Camera, CameraResultType, CameraSource} from '@capacitor/camera';
+
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  
   constructor() {}
 
+  async takePhoto() {
+    console.log('takePhoto');
+    const image = await Camera.getPhoto({
+      quality: 100,
+      allowEditing: true,
+      resultType: CameraResultType.Base64,
+      source: CameraSource.Camera
+    });
+    console.log('image', image);
+  }
 }
