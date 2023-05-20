6aa7c53cf4a6e7831ec93559f8f60017a9e7810e
Kevin <playsbykevin@gmail.com>
2023-05-21 02:23:19 +0300

    new

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import {Camera, CameraResultType, CameraSource} from '@capacitor/camera';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Camera, CameraResultType, CameraSource} from "@capacitor/camera";
+import {Component} from "@angular/core";
+
+const GOOGLE_VISION_API_URL = 'https://vision.googleapis.com/v1/images:annotate';
 
 
 @Component({
@@ -8,17 +11,54 @@ import {Camera, CameraResultType, CameraSource} from '@capacitor/camera';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  
-  constructor() {}
+
+
+  constructor(private http: HttpClient) {}
 
   async takePhoto() {
     console.log('takePhoto');
     const image = await Camera.getPhoto({
       quality: 100,
-      allowEditing: true,
+      allowEditing: false,
       resultType: CameraResultType.Base64,
       source: CameraSource.Camera
     });
     console.log('image', image);
+  }
+
+  // Method to send the base64 image to Google Cloud Vision API and extract the description
+  async processImage(base64Image: string): Promise<string> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'x-goog-user-project': USER_PROJECT,
+      Authorization: `Bearer ${GOOGLE_VISION_API_KEY}`,
+    });
+
+    const requestBody = {
+      requests: [
+        {
+          image: {
+            content: base64Image,
+          },
+          features: [
+            {
+              type: 'DOCUMENT_TEXT_DETECTION',
+            },
+          ],
+        },
+      ],
+    };
+
+    try {
+      const response = await this.http
+        .post<any>(GOOGLE_VISION_API_URL, requestBody, { headers })
+        .toPromise();
+
+      // Extract the description from the response
+      return response.responses[0].textAnnotations[0].description;
+    } catch (error) {
+      console.error('Error processing image:', error);
+      throw error;
+    }
   }
 }
